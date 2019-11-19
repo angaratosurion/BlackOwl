e99bc031d3a7383b0f2dd5102a26ddb79c809aef
angaratosurion <angarato.surion@gmail.com>
2019-11-19 16:07:35 +0200

    hh
    hh

---
+++ Projects/Verbs/ProjectVerb.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackCogs.Interfaces;
+using BlackOwl.Core.Application;
+
+namespace Projects.Verbs
+{
+    [Export(typeof(IActionVerb)), ExportMetadata("Category", "Navigation")]
+    public class ProjectVerb : IActionVerb
+    {
+        public string Action
+        {
+            get
+            {
+                return "Index";
+            }
+        }
+
+        public string Controller
+        {
+            get
+            {
+                return "Projects";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Handles Projects";
+            }
+        }
+
+        public bool isAdminPalnel
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Projects";
+            }
+        }
+        public string Moduledescription
+        {
+            get
+            {
+                Info inf = new Info();
+                return inf.Description;
+            }
+        }
+
+        public string ModuleName
+        {
+            get
+            {
+                Info inf = new Info();
+                return inf.Name;
+            }
+        }
+    }
+}
---
+++ Projects/Verbs/ProjectsAdmVerb.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackCogs.Interfaces;
+using BlackOwl.Core.Application;
+
+namespace Projects .Verbs
+{
+    [Export(typeof(IActionVerb)), ExportMetadata("Category", "Navigation")]
+    public class ProjectsAdmVerb : IActionVerb
+    {
+        public string Action
+        {
+            get
+            {
+                return "Index";
+            }
+        }
+
+        public string Controller
+        {
+            get
+            {
+                return "Projects";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Here you Administrage the Existing Projects s";
+            }
+        }
+
+        public bool isAdminPalnel
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Projects  Administration";
+            }
+        }
+        public string Moduledescription
+        {
+            get
+            {
+                Info inf = new Info();
+                return inf.Description;
+            }
+        }
+
+        public string ModuleName
+        {
+            get
+            {
+                Info inf = new Info();
+                return inf.Name;
+            }
+        }
+    }
+}
